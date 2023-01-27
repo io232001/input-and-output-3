7d89da09dfcd5d5e3f466799d965eb88fe21618b
iosbd <io232001@gmail.com>
2023-01-27 14:24:57 +0100

    Initial commit

---
+++ src/it/develhope/io/PrintYourNameAndSurname.java
@@ -0,0 +1,12 @@
+package it.develhope.io;
+
+public class PrintYourNameAndSurname
+{
+    public static void main(String arg[])
+    {
+        String name ="ionut";
+        String surname = "mazilu";
+        System.out.println(name);
+        System.out.println(surname);
+    }
+}
